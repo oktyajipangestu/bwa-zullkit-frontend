6ae31707f88583afad84a8b23de2b4e0089bc1de
Okty Aji Pangestu <oktyajipangestu@gmail.com>
2024-06-27 22:33:42 +0700

    Register & Login Components

---
+++ src/components/authentication/FeaturesComponent.vue
@@ -0,0 +1,10 @@
+<script setup>
+import CustomizableComponent from './features/CustomizableComponent.vue';
+import FortuneComponent from './features/FortuneComponent.vue';
+import DocumentationComponent from './features/CustomizableComponent.vue';
+</script>
+<template>
+    <CustomizableComponent />
+    <FortuneComponent />
+    <DocumentationComponent />
+</template>
---
+++ src/components/authentication/features/CustomizableComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mb-12">
+    <div class="block mb-2">
+      <svg
+        width="45"
+        height="45"
+        viewBox="0 0 60 60"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="inline align-middle"
+      >
+        <circle cx="30" cy="30" r="30" fill="#642DFB" />
+        <path
+          d="M20 35L30 40L40 35"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M20 30L30 35L40 30"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M30 20L20 25L30 30L40 25L30 20Z"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+      <h3 class="inline-block ml-2 text-lg font-semibold">Customizable</h3>
+    </div>
+    <p class="mt-2 text-lg text-gray-500">
+      Accelerate your work with our millions <br />
+      of ready-to-use design projects.
+    </p>
+  </div>
+</template>
